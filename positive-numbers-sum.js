37e8581904ffaf87b446be4be08d4500c57297f0
MarzukaaZaki <zakimarzuka@gmail.com>
2023-07-25 21:58:50 +0600

    Solved q2 - sum of positive numbers

---
+++ positive-numbers-sum.js
@@ -0,0 +1,11 @@
+const sumPositiveNumbers = numbers => {
+    let sum = 0;
+    for (let i=0; i<numbers.length ; i++){
+        if (numbers[i]>0){
+            sum+=numbers[i];
+        }
+    }
+    return sum;
+}
+const numbers = [2, -5, 10, -3, 7]
+console.log(sumPositiveNumbers(numbers));

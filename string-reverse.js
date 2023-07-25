1244b0ab74ca6ec6a15ad1c27ef0857649261554
MarzukaaZaki <zakimarzuka@gmail.com>
2023-07-25 21:47:54 +0600

    Solved q1 - string reverse

--- string-reverse.js
+++ string-reverse.js
@@ -4,6 +4,8 @@ const reverseString = (inputString) => {
     {
         reversedString+=inputString[i];
     }
+    return reversedString;
 }
 
-const inputString = "Hello World"; 
+const inputString = "&^$5"; 
+console.log(reverseString(inputString));
